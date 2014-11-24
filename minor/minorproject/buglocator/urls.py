9da7c0afc908361efa9cd1457d9843962ef9480b
sarthakmeh <sarthakmeh03@gmail.com>
2014-11-24 16:11:24 +0530

    Module : Uplaod a file --started

---
+++ minor/minorproject/buglocator/urls.py
@@ -0,0 +1,7 @@
+
+from django.conf.urls import patterns, url
+from buglocator import views
+
+urlpatterns = patterns('buglocator.views',
+    url(r'^list/$','list'),
+)
--- minor/minorproject/buglocator/views.py
+++ minor/minorproject/buglocator/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+#from myproject.myapp.forms import DocumentForm
 
-# Create your views here.
+def list(request):
+    # Handle file upload
+    
+    # Render list page with the documents and the form
+    return render(request,
+        'buglocator/list.html',
+        {}  )
