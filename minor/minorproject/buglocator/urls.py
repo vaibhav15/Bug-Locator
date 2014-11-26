6dfa0470bfca705cd6bae3b0204823896b47ba07
sarthakmeh <sarthakmeh03@gmail.com>
2014-11-26 15:09:50 +0530

    login and registration added

--- minor/minorproject/buglocator/urls.py
+++ minor/minorproject/buglocator/urls.py
@@ -4,4 +4,6 @@ from buglocator import views
 
 urlpatterns = patterns('buglocator.views',
     url(r'^list/$','list',name='list'),
+    url(r'^login/$','login',name='login'),
+    url(r'^registration/$','registration',name='registration'),
 )
--- minor/minorproject/buglocator/views.py
+++ minor/minorproject/buglocator/views.py
@@ -3,6 +3,8 @@ from forms import DocumentForm
 from models import Document
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.http import HttpResponse
+
 
 def list(request):
     # Handle file upload
@@ -19,3 +21,32 @@ def list(request):
        documents = Document.objects.all()
        # Render list page with the documents and the form
        return render(request,'buglocator/list.html',{'form':form,'documents':documents}  )
+
+def registration(request):
+    if request.method=='POST':
+       try:
+           user = User.objects.get(email = request.DATA["email"])
+       except:
+          post_values = request.DATA.copy()
+          password = post_values["password"]
+          post_values["password"]= make_password(password)
+          user = User(username=request.DATA["username"],email=request.DATA["email"],password=post_values["password"])   
+          user.save()
+          return HttpResponse("Registration successfull")
+       return HttpResponse("Registration unsuccessfull")
+                                                   
+def login(request):
+    if request.method=="POST":
+       try :
+           user=User.objects.get(email=request.DATA["email"])
+       except:
+           return HttpResponse("User not found")
+    
+       if make_password(request.DATA["password"],user.password):
+          return HttpResponse("Login successfull")
+       return HttpResponse("Login unsuccessfull") 
+
+       
+
+             
+
